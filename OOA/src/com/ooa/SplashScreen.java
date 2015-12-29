c14851ad66edccdaca77ef4e69254f40e2c5f700
Saurabh <Saurabh@Saurabh-PC>
2015-12-29 13:45:16 +0530

    second commit

--- OOA/src/com/ooa/SplashScreen.java
+++ OOA/src/com/ooa/SplashScreen.java
@@ -10,30 +10,24 @@ public class SplashScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
-		 Thread timerThread = new Thread()
-		 {
-	            public void run()
-	            {
-	                try
-	                {
-	                    sleep(2000);
-	                }catch(InterruptedException e)
-	                {
-	                    e.printStackTrace();
-	                }//finally
-	               /* {
-	                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
-	                    startActivity(i);
-	                }*/
-	            }
-	        };
-	        timerThread.start();
-	    }
-	 
-	    @Override
-	    protected void onPause() {
-	        // TODO Auto-generated method stub
-	        super.onPause();
-	        finish();
-	    }
+		Thread timerThread = new Thread() {
+			public void run() {
+				try {
+					sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} 
+			}
+		};
+		timerThread.start();
+		
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
 }
