316ed795685cf80c718acb7c236a3be223db6e62
Saurabh <Saurabh@Saurabh-PC>
2015-12-31 12:18:15 +0530

    commit 31-12-2015

--- OOA/src/com/ooa/MainActivity.java
+++ OOA/src/com/ooa/MainActivity.java
@@ -7,30 +7,40 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity 
-{
-	Button btn_login,btn_booking;
+public class MainActivity extends Activity {
+	Button btn_login, btn_booking;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		getActionBar().hide();
-		
+
 		initial();
-	
+
 	}
 
 	private void initial() {
 		// TODO Auto-generated method stub
-		btn_login=(Button)findViewById(R.id.btn_login); 
-		btn_booking=(Button)findViewById(R.id.btn_booking);
-	
+		btn_login = (Button) findViewById(R.id.btn_login);
+		btn_booking = (Button) findViewById(R.id.btn_booking);
+
 		btn_login.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent mIntent=new Intent(MainActivity.this,LoginActivity.class);
+				Intent mIntent = new Intent(MainActivity.this, LoginActivity.class);
+				startActivity(mIntent);
+			}
+		});
+		btn_booking.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				Intent mIntent = new Intent(MainActivity.this, MakeBooking.class);
 				startActivity(mIntent);
 			}
 		});
