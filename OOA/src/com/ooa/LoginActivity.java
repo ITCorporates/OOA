1fb43095ce5fe3586820d548f081177919ea6b5b
Saurabh <Saurabh@Saurabh-PC>
2016-01-07 12:22:27 +0530

    Commit 1-7-2016

--- OOA/src/com/ooa/LoginActivity.java
+++ OOA/src/com/ooa/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.ooa;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements RestApiCallListener {
 	EditText email, password;
+	private String response;
 	private String emailInput;
 	Button btn_login;
 
@@ -34,19 +39,101 @@ public class LoginActivity extends Activity {
 				// Toast.LENGTH_SHORT).show();
 				emailInput = email.getText().toString().trim();
 
-				String emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-				if (emailInput.matches(emailPattern)) {
+				if (emailInput.matches(Constants.EmailPattern)) {
 					Toast.makeText(LoginActivity.this, "valid email address", Toast.LENGTH_SHORT).show();
 
 				} else {
 					Toast.makeText(LoginActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
 					// email.setBackgroundResource(R.drawable.edit_red_line);
 				}
+				login();
+			}
+
+			private void login() {
+				// TODO Auto-generated method stub
+
+				// Set the User name and password in preferences when toggle is
+				// on
+
+				try {
+					JSONObject JsonObject = new JSONObject();
+					JsonObject.put("username", email.getText().toString());
+					JsonObject.put("password", password.getText().toString());
+
+					RestApiCallPost apiCallPost = new RestApiCallPost(Constants.LOGIN, LoginActivity.this, JsonObject,
+							1);
+					apiCallPost.start();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 
 			}
 		});
 
 	}
+
+	@Override
+	public void onResponse(String response, int pageId) {
+		// TODO Auto-generated method stub
+		this.response = response;
+
+		switch (pageId) {
+		case 1:
+			Log.e("Response of Passenger Login", response);
+			handler.sendEmptyMessage(pageId);
+
+			break;
+		}
+
+	}
+
+	@Override
+	public void onError(String error) {
+		// TODO Auto-generated method stub
+
+	}
+
+	Handler handler = new Handler() {
+
+		public void handleMessage(android.os.Message msg) {
+
+			switch (msg.what) {
+
+			case 1:
+				// "appVersionrequired"
+				try {
+					JSONObject jsonObject = new JSONObject(response);
+
+					if (jsonObject.getString("status").equals("ok")) {
+						Log.e("Success", "Welcome ");
+
+					} else if (jsonObject.getString("status").equals("error")) {
+						Log.e("ERROR", "NO Response");
+					}
+
+					/*
+					 * else if(jsonObject.getString("message").equals(
+					 * "Invalid credentials supplied or activation is pending"))
+					 * { Utils.getAlertDialog(PassengerMainActivity.this,
+					 * "Invalid credentials supplied or activation is pending",
+					 * new Handler()).show(); }
+					 */
+
+					else if (jsonObject.getString("status").equals("alreadyloggedin")) {
+					}
+
+					else if (jsonObject.getString("message").equals(
+							"Please use your registered device or call your system administrator for support.")) {
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+	};
 
 }
--- OOA/src/com/ooa/MakeBooking.java
+++ OOA/src/com/ooa/MakeBooking.java
@@ -1,9 +1,20 @@
 package com.ooa;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MakeBooking extends Activity {
+	AlertDialog dialogDetails = null;
+	Button route;
+	TextView Chd2Moh;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +22,40 @@ public class MakeBooking extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.make_booking);
 		getActionBar().hide();
+
+		route = (Button) findViewById(R.id.route);
+
+		route.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				LayoutInflater inflater = LayoutInflater.from(v.getContext());
+				View dialogview = inflater.inflate(R.layout.routelist, null);
+
+				AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(v.getContext());
+
+				dialogbuilder.setView(dialogview);
+				dialogDetails = dialogbuilder.create();
+				dialogDetails.show();
+				Chd2Moh = (TextView) dialogDetails.findViewById(R.id.Chd2Moh);
+				Chd2Moh.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						// TODO Auto-generated method stub
+						// image1.setImageResource(R.drawable.cash);
+
+						dialogDetails.dismiss();
+						route.setText("\t" + "Chandigarh To Mohali");
+
+
+					}
+				});
+
+			}
+		});
+
 	}
 }
